9b579be0155d2694a138141032a7f368baf7e25f
chenao <chenao1010@foxmail.com>
2014-11-02 19:38:59 +0800

    1111

---
+++ Stock/src/Data/StockData.java
@@ -0,0 +1,18 @@
+package Data;
+
+public class StockData {
+	String uid;
+	String name;
+	String rise;
+	String nowprice;
+	String openprice;
+	String closeprice;
+	String buyprice;
+	String saleprice;
+	String oknumber;
+	String highprice;
+	String lowprice;
+	String date;
+	String todayrate;
+	String tomrate;
+}
